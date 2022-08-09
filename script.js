c35871387ca9a845f0dd20dcde6054cfb61f68e8
unknown <k.ekoptr@gmail.com>
2022-08-09 10:38:50 +0700

    project oprec ipj

---
+++ script.js
@@ -0,0 +1,22 @@
+// hamburger active
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    const nav = document.querySelector('#nav');
+    nav.classList.toggle('hidden');
+})
+
+//navbar fixed
+
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    }
+    else {
+        header.classList.remove('navbar-fixed');
+    }
+}
